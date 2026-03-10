cacf54081dfa91dec3b5499bb5708b77bf292ac0
Yosef <yavitan1234@gmail.com>
2026-03-10 20:11:44 +0200

    fix: next is not a function

--- api/models/User.js
+++ api/models/User.js
@@ -11,10 +11,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('passwordHash')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('passwordHash')) return;
   this.passwordHash = await bcrypt.hash(this.passwordHash, 10);
-  next();
 });
 
 userSchema.methods.comparePassword = function (candidate) {
